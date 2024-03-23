87a05bb1f787c619f5b758e466da2a8478df7ecb
Najiullah7 <128100842+Najiullah7@users.noreply.github.com>
2024-03-22 21:49:16 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,16 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+import demo
 
+# gets the data
+df = pd.read_csv("pokematch.csv")
+
+# gets matrix of terms and docs
+term_mat = pd.read_csv('td_mat.csv').values.tolist()
+
+# gets list of good types. idk why the 0 is there, it just adds it.
+good_types = pd.read_csv('goodtypes.csv')['0'].tolist()
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
@@ -13,33 +22,36 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'init.json')
+# json_file_path = os.path.join(current_directory, 'pokematch.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
+# with open(json_file_path, 'r') as file:
+#     data = json.load(file)
+#     pokedex_df = pd.DataFrame(data['pokedex'])
 
 app = Flask(__name__)
 CORS(app)
 
 # Sample search using json with pandas
 def json_search(query):
-    matches = []
-    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    matches_filtered_json = matches_filtered.to_json(orient='records')
-    return matches_filtered_json
+
+    print("I am in json search")
+
+    s = query
+    k = 6
+
+    answer = demo.top_k(s,term_mat,good_types,k,df)[['name','desc']]
+    return answer.to_json(orient='records')
+
 
 @app.route("/")
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
-def episodes_search():
+@app.route("/pokemon")
+def pokemon_search():
     text = request.args.get("title")
+    print(json_search(text))
     return json_search(text)
 
 if 'DB_NAME' not in os.environ:
