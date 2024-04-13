ed3082ce6c5099c01ca0e0bae2bd43fa8531816c
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2024-04-13 12:25:49 -0400

    Clean up app.py

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
 import demo
+import re
 
 # gets the data
 df = pd.read_csv("pokematch.csv")
@@ -14,35 +15,15 @@ term_mat = pd.read_csv('td_mat.csv').values.tolist()
 
 # gets list of good types. idk why the 0 is there, it just adds it.
 good_types = pd.read_csv('goodtypes.csv')['0'].tolist()
-# ROOT_PATH for linking with all your files. 
-# Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
-
-# Get the directory of the current script
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Specify the path to the JSON file relative to the current script
-# json_file_path = os.path.join(current_directory, 'pokematch.json')
-
-# Assuming your JSON data is stored in a file named 'init.json'
-# with open(json_file_path, 'r') as file:
-#     data = json.load(file)
-#     pokedex_df = pd.DataFrame(data['pokedex'])
 
 app = Flask(__name__)
 CORS(app)
 
-# Sample search using json with pandas
 def json_search(query):
-
-    print("I am in json search")
-
     s = query
     k = 6
-
-    answer = demo.top_k(s,term_mat,good_types,k,df)[['name','desc']]
+    answer = demo.top_k(s,term_mat,good_types,k,df)[['name','desc', 'pop']]
     return answer.to_json(orient='records')
-
 
 @app.route("/")
 def home():
@@ -51,8 +32,8 @@ def home():
 @app.route("/pokemon")
 def pokemon_search():
     text = request.args.get("title")
-    print(json_search(text))
     return json_search(text)
+
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True,host="0.0.0.0",port=5000)
