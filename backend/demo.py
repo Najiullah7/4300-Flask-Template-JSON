2464158b311e7d1ad1fc1d55483bdd79ddeeb6c8
ZakirBAhmad <35638421+ZakirBAhmad@users.noreply.github.com>
2024-04-15 10:00:22 -0400

    Update demo.py
    Added imports

--- backend/demo.py
+++ backend/demo.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import normalize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse.linalg import svds
 import re
 
 #--------------- Functions ------------------------------
